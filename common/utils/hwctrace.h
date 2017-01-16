kalyankondapally/hwcomposer

Initial import.

common/utils/hwctrace.h
@@ -1,0 +1,230 @@
+/*
+// Copyright (c) 2016 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#ifndef HWC_TRACE_H
+#define HWC_TRACE_H
+
+#include <string>
+#include <errno.h>
+#include <stdio.h>
+#include <chrono>
+#include <time.h>
+
+#include <platformdefines.h>
+
+#include "displayplane.h"
+
+#ifdef _cplusplus
+extern "C" {
+#endif
+
+//#define ENABLE_DEBUG 1
+//#define ENABLE_DISPLAY_DUMP 1
+//#define ENABLE_DISPLAY_MANAGER_TRACING 1
+//#define ENABLE_PAGE_FLIP_EVENT_TRACING 1
+//#define ENABLE_HOT_PLUG_EVENT_TRACING 1
+//#define FUNCTION_CALL_TRACING 1
+#define COMPOSITOR_TRACING 1
+
+// Helper to automatically preappend classname::functionname to the log message
+#ifdef ENABLE_DEBUG
+#define VTRACE(fmt, ...) VLOG("%s: " fmt, __func__, ##__VA_ARGS__)
+#define DTRACE(fmt, ...) DLOG("%s: " fmt, __func__, ##__VA_ARGS__)
+#define ITRACE(fmt, ...) ILOG("%s: " fmt, __func__, ##__VA_ARGS__)
+#define WTRACE(fmt, ...) WLOG("%s: " fmt, __func__, ##__VA_ARGS__)
+#else
+#define VTRACE(fmt, ...) ((void)0)
+#define DTRACE(fmt, ...) ((void)0)
+#define ITRACE(fmt, ...) ((void)0)
+#define WTRACE(fmt, ...) ((void)0)
+#endif
+#define ETRACE(fmt, ...) ELOG("%s: " fmt, __func__, ##__VA_ARGS__)
+
+// Function call tracing
+#ifdef FUNCTION_CALL_TRACING
+class TraceFunc {
+ public:
+  TraceFunc(std::string func_name) {
+    func_name_ = func_name;
+    ILOG("Calling ----- %s", func_name_.c_str());
+    t_ = std::chrono::high_resolution_clock::now();
+  }
+  ~TraceFunc() {
+    std::chrono::high_resolution_clock::time_point t2 =
+        std::chrono::high_resolution_clock::now();
+    ILOG(
+        "Total time spent in --- %s Time(msec): %lld", func_name_.c_str(),
+        std::chrono::duration_cast<std::chrono::microseconds>(t2 - t_).count());
+    ILOG("Leaving --- %s", func_name_.c_str());
+  }
+
+ private:
+  std::chrono::high_resolution_clock::time_point t_;
+  std::string func_name_;
+};
+#define CTRACE() TraceFunc hwctrace(__func__);
+#else
+#define CTRACE() ((void)0)
+#endif
+
+// Arguments tracing
+#if 0
+#define ATRACE(fmt, ...) VTRACE("%s(args): " fmt, __func__, ##__VA_ARGS__);
+#else
+#define ATRACE(fmt, ...) ((void)0)
+#endif
+
+// Useful Debug tracing
+#ifdef ENABLE_DISPLAY_DUMP
+#define DUMPTRACE(fmt, ...) ILOG(fmt, ##__VA_ARGS__)
+#else
+#define DUMPTRACE(fmt, ...) ((void)0)
+#endif
+
+// Page Flip event tracing
+#ifdef ENABLE_PAGE_FLIP_EVENT_TRACING
+#define IPAGEFLIPEVENTTRACE(fmt, ...) ILOG(fmt, ##__VA_ARGS__)
+#else
+#define IPAGEFLIPEVENTTRACE(fmt, ...) ((void)0)
+#endif
+
+#ifdef ENABLE_DISPLAY_MANAGER_TRACING
+#define IDISPLAYMANAGERTRACE(fmt, ...) ILOG(fmt, ##__VA_ARGS__)
+#else
+#define IDISPLAYMANAGERTRACE(fmt, ...) ((void)0)
+#endif
+
+#ifdef ENABLE_HOT_PLUG_EVENT_TRACING
+#define IHOTPLUGEVENTTRACE(fmt, ...) ILOG(fmt, ##__VA_ARGS__)
+#else
+#define IHOTPLUGEVENTTRACE(fmt, ...) ((void)0)
+#endif
+
+#ifdef COMPOSITOR_TRACING
+#define ICOMPOSITORTRACE(fmt, ...) ILOG(fmt, ##__VA_ARGS__)
+#else
+#define ICOMPOSITORTRACE(fmt, ...) ((void)0)
+#endif
+
+// Errors
+#define PRINTERROR() strerror(-errno)
+
+// Helper to abort the execution if object is not initialized.
+// This should never happen if the rules below are followed during design:
+// 1) Create an object.
+// 2) Initialize the object immediately.
+// 3) If failed, delete the object.
+// These helpers should be disabled and stripped out of release build
+
+#define RETURN_X_IF_NOT_INIT(X)                                              \
+  do {                                                                       \
+    CTRACE();                                                                \
+    if (false == mInitialized) {                                             \
+      LOG_ALWAYS_FATAL("%s: Object is not initialized! Line = %d", __func__, \
+                       __LINE__);                                            \
+      return X;                                                              \
+    }                                                                        \
+  } while (0)
+
+#if 1
+#define RETURN_FALSE_IF_NOT_INIT() RETURN_X_IF_NOT_INIT(false)
+#define RETURN_VOID_IF_NOT_INIT() RETURN_X_IF_NOT_INIT()
+#define RETURN_NULL_IF_NOT_INIT() RETURN_X_IF_NOT_INIT(0)
+#else
+#define RETURN_FALSE_IF_NOT_INIT() ((void)0)
+#define RETURN_VOID_IF_NOT_INIT() ((void)0)
+#define RETURN_NULL_IF_NOT_INIT() ((void)0)
+#endif
+
+// Helper to log error message, call de-initializer and return false.
+#define DEINIT_AND_RETURN_FALSE(...) \
+  do {                               \
+    ETRACE(__VA_ARGS__);             \
+    deinitialize();                  \
+    return false;                    \
+  } while (0)
+
+#define DEINIT_AND_DELETE_OBJ(X) \
+  if (X) {                       \
+    X->deinitialize();           \
+    delete X;                    \
+    X = NULL;                    \
+  }
+
+#define WARN_IF_NOT_DEINIT()                                                 \
+  CTRACE();                                                                  \
+  if (mInitialized) {                                                        \
+    LOG_ALWAYS_FATAL("%s: Object is not deinitialized! Line = %d", __func__, \
+                     __LINE__);                                              \
+  }
+
+#ifdef ENABLE_DISPLAY_DUMP
+#define DUMP_CURRENT_COMPOSITION_PLANES()                                  \
+  frame_++;                                                                \
+  DUMPTRACE(                                                               \
+      "Dumping DisplayPlaneState of Current Composition planes "           \
+      "-----------------------------");                                    \
+  DUMPTRACE("Frame: %d", frame_);                                          \
+  DUMPTRACE("Total Layers for this Frame: %d", layers.size());             \
+  DUMPTRACE("Total Planes in use for this Frame: %d",                      \
+            current_composition_planes.size());                            \
+  int plane_index = 1;                                                     \
+  for (DisplayPlaneState & comp_plane : current_composition_planes) {      \
+    DUMPTRACE("Composition Plane State for Index: %d", plane_index);       \
+    const std::vector<size_t> &source_layers = comp_plane.source_layers(); \
+    switch (comp_plane.GetCompositionState()) {                            \
+      case DisplayPlaneState::State::kRender:                              \
+        DUMPTRACE("DisplayPlane state: kRender. Total layers: %lu",        \
+                  source_layers.size());                                   \
+        DUMPTRACE("Layers Index:");                                        \
+        for (size_t primary_index : source_layers) {                       \
+          DUMPTRACE("index: %d", primary_index);                           \
+          layers.at(primary_index).Dump();                                 \
+        }                                                                  \
+        break;                                                             \
+      case DisplayPlaneState::State::kScanout:                             \
+        if (source_layers.size() > 1)                                      \
+          DUMPTRACE(                                                       \
+              "Plane has more than one layer associated when its type is " \
+              "kScanout. This needs to be fixed.");                        \
+        DUMPTRACE("DisplayPlane State: kScanout. Total layers: %lu",       \
+                  source_layers.size());                                   \
+        DUMPTRACE("Layers Index:");                                        \
+        for (size_t overlay_index : source_layers) {                       \
+          DUMPTRACE("index: %d", overlay_index);                           \
+          layers.at(overlay_index).Dump();                                 \
+        }                                                                  \
+        break;                                                             \
+      default:                                                             \
+        break;                                                             \
+    }                                                                      \
+    comp_plane.plane()->Dump();                                            \
+    DUMPTRACE("Composition Plane State ends for Index: %d", plane_index);  \
+    plane_index++;                                                         \
+  }                                                                        \
+  DUMPTRACE(                                                               \
+      "Dumping DisplayPlaneState of Current Composition planes ends. "     \
+      "-----------------------------");
+#else
+#define DUMP_CURRENT_COMPOSITION_PLANES() ((void)0)
+#endif
+
+// _cplusplus
+#ifdef _cplusplus
+}
+#endif
+
+#endif /* HWC_TRACE_H */

kalyankondapally/hwcomposer-1

Includes Several commits:
commit aee43ab680941f5c7322cd62abb91b1ddf398852
Author: Kalyan Kondapally <kalyan.kondapally@intel.com>
Date:   Sat Jan 14 23:35:35 2017 -0800
    Fix issues related to Tests.
    This fixes the linking issues we had with some of the GL
    and EGL API. Test apps build and link but Atomic commit fails
    during run time.
    Jira: None.
    Test: Builds on Linux and Android. No regressions seen on Android.
    Signed-off-by: Kalyan Kondapally <kalyan.kondapally@intel.com>
commit 8ff76e215cfdb08a92382a11ea15ac3188ddf6fa
Date:   Sat Jan 14 16:47:26 2017 -0800
    Use sync wait instead of waiting in a loop to submit commit.
    Inspired by: https://github.com/rantogno/kmscube/commit/fc52268da79ed406da02c88f849f1ef70dab789f
    Test: Builds and boots to home screen. No Graphics regrssion.
commit 3aa1ae042b9ccd7c4f768db0498a056b92903f71
Author: Johnson Lin <johnson.lin@intel.com>
Date:   Thu Jan 12 10:58:08 2017 +0800
    Add kmscube as test program
    Test program doesn't link yet. Imported from
    https://github.com/rantogno/kmscube
    Test: None.
commit 0f7cfbc9bc8fcec3e296433ac9d09e549cc4967c
Author: Tapani Pälli <tapani.palli@intel.com>
Date:   Tue Jan 10 06:58:27 2017 -0800
    hwcomposer: in case of EBUSY, spin until commit succeeded
    Bug: #11
    Test: AnTuTu Marooned demo works correctly
    Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
Initial Linux support added by Johnson Lin <johnson.lin@intel.com>

common/utils/hwctrace.h
@@ -1,0 +1,215 @@
+/*
+// Copyright (c) 2016 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#ifndef HWC_TRACE_H
+#define HWC_TRACE_H
+
+#include <string>
+#include <errno.h>
+#include <stdio.h>
+#include <chrono>
+#include <time.h>
+
+#include "displayplane.h"
+
+#include <platformdefines.h>
+
+#ifdef _cplusplus
+extern "C" {
+#endif
+
+//#define ENABLE_DISPLAY_DUMP 1
+//#define ENABLE_DISPLAY_MANAGER_TRACING 1
+//#define ENABLE_PAGE_FLIP_EVENT_TRACING 1
+//#define ENABLE_HOT_PLUG_EVENT_TRACING 1
+//#define FUNCTION_CALL_TRACING 1
+#define COMPOSITOR_TRACING 1
+
+// Function call tracing
+#ifdef FUNCTION_CALL_TRACING
+class TraceFunc {
+ public:
+  TraceFunc(std::string func_name) {
+    func_name_ = func_name;
+    ILOG("Calling ----- %s", func_name_.c_str());
+    t_ = std::chrono::high_resolution_clock::now();
+  }
+  ~TraceFunc() {
+    std::chrono::high_resolution_clock::time_point t2 =
+        std::chrono::high_resolution_clock::now();
+    ILOG(
+        "Total time spent in --- %s Time(msec): %lld", func_name_.c_str(),
+        std::chrono::duration_cast<std::chrono::microseconds>(t2 - t_).count());
+    ILOG("Leaving --- %s", func_name_.c_str());
+  }
+
+ private:
+  std::chrono::high_resolution_clock::time_point t_;
+  std::string func_name_;
+};
+#define CTRACE() TraceFunc hwctrace(__func__);
+#else
+#define CTRACE() ((void)0)
+#endif
+
+// Arguments tracing
+#if 0
+#define ATRACE(fmt, ...) VTRACE("%s(args): " fmt, __func__, ##__VA_ARGS__);
+#else
+#define ATRACE(fmt, ...) ((void)0)
+#endif
+
+// Useful Debug tracing
+#ifdef ENABLE_DISPLAY_DUMP
+#define DUMPTRACE(fmt, ...) ITRACE(fmt, ##__VA_ARGS__)
+#else
+#define DUMPTRACE(fmt, ...) ((void)0)
+#endif
+
+// Page Flip event tracing
+#ifdef ENABLE_PAGE_FLIP_EVENT_TRACING
+#define IPAGEFLIPEVENTTRACE(fmt, ...) ITRACE(fmt, ##__VA_ARGS__)
+#else
+#define IPAGEFLIPEVENTTRACE(fmt, ...) ((void)0)
+#endif
+
+#ifdef ENABLE_DISPLAY_MANAGER_TRACING
+#define IDISPLAYMANAGERTRACE(fmt, ...) ITRACE(fmt, ##__VA_ARGS__)
+#else
+#define IDISPLAYMANAGERTRACE(fmt, ...) ((void)0)
+#endif
+
+#ifdef ENABLE_HOT_PLUG_EVENT_TRACING
+#define IHOTPLUGEVENTTRACE(fmt, ...) ITRACE(fmt, ##__VA_ARGS__)
+#else
+#define IHOTPLUGEVENTTRACE(fmt, ...) ((void)0)
+#endif
+
+#ifdef COMPOSITOR_TRACING
+#define ICOMPOSITORTRACE(fmt, ...) ITRACE(fmt, ##__VA_ARGS__)
+#else
+#define ICOMPOSITORTRACE(fmt, ...) ((void)0)
+#endif
+
+// Errors
+#define PRINTERROR() strerror(-errno)
+
+// Helper to abort the execution if object is not initialized.
+// This should never happen if the rules below are followed during design:
+// 1) Create an object.
+// 2) Initialize the object immediately.
+// 3) If failed, delete the object.
+// These helpers should be disabled and stripped out of release build
+
+#define RETURN_X_IF_NOT_INIT(X)                                              \
+  do {                                                                       \
+    CTRACE();                                                                \
+    if (false == mInitialized) {                                             \
+      LOG_ALWAYS_FATAL("%s: Object is not initialized! Line = %d", __func__, \
+                       __LINE__);                                            \
+      return X;                                                              \
+    }                                                                        \
+  } while (0)
+
+#if 1
+#define RETURN_FALSE_IF_NOT_INIT() RETURN_X_IF_NOT_INIT(false)
+#define RETURN_VOID_IF_NOT_INIT() RETURN_X_IF_NOT_INIT()
+#define RETURN_NULL_IF_NOT_INIT() RETURN_X_IF_NOT_INIT(0)
+#else
+#define RETURN_FALSE_IF_NOT_INIT() ((void)0)
+#define RETURN_VOID_IF_NOT_INIT() ((void)0)
+#define RETURN_NULL_IF_NOT_INIT() ((void)0)
+#endif
+
+// Helper to log error message, call de-initializer and return false.
+#define DEINIT_AND_RETURN_FALSE(...) \
+  do {                               \
+    ETRACE(__VA_ARGS__);             \
+    deinitialize();                  \
+    return false;                    \
+  } while (0)
+
+#define DEINIT_AND_DELETE_OBJ(X) \
+  if (X) {                       \
+    X->deinitialize();           \
+    delete X;                    \
+    X = NULL;                    \
+  }
+
+#define WARN_IF_NOT_DEINIT()                                                 \
+  CTRACE();                                                                  \
+  if (mInitialized) {                                                        \
+    LOG_ALWAYS_FATAL("%s: Object is not deinitialized! Line = %d", __func__, \
+                     __LINE__);                                              \
+  }
+
+#ifdef ENABLE_DISPLAY_DUMP
+#define DUMP_CURRENT_COMPOSITION_PLANES()                                  \
+  frame_++;                                                                \
+  DUMPTRACE(                                                               \
+      "Dumping DisplayPlaneState of Current Composition planes "           \
+      "-----------------------------");                                    \
+  DUMPTRACE("Frame: %d", frame_);                                          \
+  DUMPTRACE("Total Layers for this Frame: %d", layers.size());             \
+  DUMPTRACE("Total Planes in use for this Frame: %d",                      \
+            current_composition_planes.size());                            \
+  int plane_index = 1;                                                     \
+  for (DisplayPlaneState & comp_plane : current_composition_planes) {      \
+    DUMPTRACE("Composition Plane State for Index: %d", plane_index);       \
+    const std::vector<size_t> &source_layers = comp_plane.source_layers(); \
+    switch (comp_plane.GetCompositionState()) {                            \
+      case DisplayPlaneState::State::kRender:                              \
+        DUMPTRACE("DisplayPlane state: kRender. Total layers: %lu",        \
+                  source_layers.size());                                   \
+        DUMPTRACE("Layers Index:");                                        \
+        for (size_t primary_index : source_layers) {                       \
+          DUMPTRACE("index: %d", primary_index);                           \
+          layers.at(primary_index).Dump();                                 \
+        }                                                                  \
+        break;                                                             \
+      case DisplayPlaneState::State::kScanout:                             \
+        if (source_layers.size() > 1)                                      \
+          DUMPTRACE(                                                       \
+              "Plane has more than one layer associated when its type is " \
+              "kScanout. This needs to be fixed.");                        \
+        DUMPTRACE("DisplayPlane State: kScanout. Total layers: %lu",       \
+                  source_layers.size());                                   \
+        DUMPTRACE("Layers Index:");                                        \
+        for (size_t overlay_index : source_layers) {                       \
+          DUMPTRACE("index: %d", overlay_index);                           \
+          layers.at(overlay_index).Dump();                                 \
+        }                                                                  \
+        break;                                                             \
+      default:                                                             \
+        break;                                                             \
+    }                                                                      \
+    comp_plane.plane()->Dump();                                            \
+    DUMPTRACE("Composition Plane State ends for Index: %d", plane_index);  \
+    plane_index++;                                                         \
+  }                                                                        \
+  DUMPTRACE(                                                               \
+      "Dumping DisplayPlaneState of Current Composition planes ends. "     \
+      "-----------------------------");
+#else
+#define DUMP_CURRENT_COMPOSITION_PLANES() ((void)0)
+#endif
+
+// _cplusplus
+#ifdef _cplusplus
+}
+#endif
+
+#endif /* HWC_TRACE_H */